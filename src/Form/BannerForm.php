5698224c3f570f797b4a3f42daea9e77ffbe663d
Alikdex <alikdex@gmail.com>
2019-02-04 15:26:33 +0300

    Set defaults

--- src/Form/BannerForm.php
+++ src/Form/BannerForm.php
@@ -22,8 +22,10 @@ class BannerForm extends Model
     {
         parent::__construct($config);
 
+        // defaults
         $this->desktop = 1;
         $this->mobile = 1;
+        $this->enabled = 0;
         $this->start_at = null;
         $this->end_at = null;
     }
@@ -50,6 +52,13 @@ class BannerForm extends Model
                 return StringHelper::truncate($attribute, 255, false);
             }],
             [['name', 'comment', 'code'], 'trim'],
+            
+            // defaults
+            ['comment', 'default', 'value' => ''],
+            ['code', 'default', 'value' => ''],
+            ['desktop', 'default', 'value' => 1],
+            ['mobile', 'default', 'value' => 1],
+            ['enabled', 'default', 'value' => 0],
             ['start_at', 'default', 'value' => null],
             ['end_at', 'default', 'value' => null],
 
